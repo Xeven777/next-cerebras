c84c9fc281e47340adee81069075a8c77f8a96b5
Anish Biswas <xevenbiswas@gmail.com>
2026-02-08 16:08:36 +0000

    update models

--- lib/model.ts
+++ lib/model.ts
@@ -1,11 +1,6 @@
 export const models = [
-  {
-    value: "qwen-3-235b-a22b-instruct-2507",
-    label: "🐦 Qwen 3 - 235B A22B Instruct 2507",
-  },
   { value: "gpt-oss-120b", label: "🤖 GPT OSS - 120B" },
   { value: "qwen-3-32b", label: "🐦 Qwen 3 - 32B" },
   { value: "llama-3.3-70b", label: "🦙 Llama 3.3 - 70B" },
-  { value: "llama3.1-8b", label: "⚡ Llama 3.1 - 8B" },
-  {value:"zai-glm-4.7",label:"🤖 Z.ai GLM 4.7"}
+  { value: "llama3.1-8b", label: "⚡ Llama 3.1 - 8B" }
 ];
